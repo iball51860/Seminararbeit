eb3c0e9f0d0b675fc0d4ec2d8f55efb32a98ad5b
manuelka <manuelkaiser92@web.de>
2013-12-06 16:38:57 +0100

    WMServer erstellt

--- Seminararbeit/src/control/WMServer.java
+++ Seminararbeit/src/control/WMServer.java
@@ -1,5 +1,60 @@
 package control;
 
-public class WMServer {
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+import model.*;
+
+
+/**
+ * 
+ * Server Class 
+ *
+ */
+public class WMServer 
+{
+	
+	private int port;
+	private boolean waiting = true;
+	
+	private ServerSocket serverSocket;
+	private TreeSet<Team> teams;
+	
+	
+	
+	public WMServer (int port)
+	{
+		this.port = port;
+		TreeSet<Team> teams = new TreeSet<Team>();
+	}
+	
+	/**
+	 * Server gets initialized and accepts Clients
+	 */
+	public void start() 
+	{
+		try 
+		{
+			serverSocket = new ServerSocket(port);
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		
+		while(waiting) 
+		{
+			Team t = new Team(serverSocket.accept());
+			teams.add(t);
+		}
+	}
+	
+	
+	public void setWaiting(boolean waiting){
+		this.waiting = waiting;
+	}	
+	
+	
 }
