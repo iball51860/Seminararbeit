5c629be2f39e9c3993d47596a6d2dbb83773d94c
manuelka <manuelkaiser92@web.de>
2013-12-09 22:44:44 +0100

    TestClients created

---
+++ Seminararbeit/src/testClient/MainClass.java
@@ -0,0 +1,24 @@
+package testClient;
+
+public class MainClass {
+	
+	
+	public static void main(String[] args) 
+	{
+		
+		TestClient[] dummy = new TestClient[10];
+		
+		for(int i = 0; i < dummy.length; i++)
+		{
+			dummy[i] = new TestClient();
+		}
+		
+		for(int i = 0; i < dummy.length; i++)
+		{
+			dummy[i].start();
+		}
+		
+		
+	}
+
+}
