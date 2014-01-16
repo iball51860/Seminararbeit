8e7134acb2f26b2966f3f506f1f49f41981a8b66
iball51860 <Jan@192.168.2.249>
2014-01-16 14:29:54 +0100

    Added prototype implementation of portcheck

--- Seminararbeit/src/viewControl/StartServerListener.java
+++ Seminararbeit/src/viewControl/StartServerListener.java
@@ -6,6 +6,9 @@ import control.WMServer;
 
 import view.*;
 
+import java.io.*;
+import java.net.*;
+
 public class StartServerListener implements ActionListener {
 	
 	
@@ -24,15 +27,29 @@ public class StartServerListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) 
 	{
 		int port = 4444;
+		boolean portIsFree = false;
 		try
 		{
 			port = Integer.valueOf(popup.getInput().getText());
+			
+		//	Socket socket = new Socket("localhost", port); //check if server is already running on port
+		//	socket.close();
 		}
 		catch(NumberFormatException nfe)
 		{
 			popup.getInput().setText("4444");
 			return;
 		}
+		/*catch(ConnectException ce){
+			portIsFree = true;
+		}
+		catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+		if(!portIsFree)
+		{
+			popup.getInput().setText("4444");
+		}*/
 		popup.dispose();
 		serverWindow.setEnabled(true);
 		serverWindow.setWMServer(new WMServer(port, serverWindow));
